7b87f0e2955ae995d4e511aa647f7fa8045e7ee3
Moritz Stückler <stueckler@ubilabs.net>
2021-02-04 16:49:34 +0100

    feat: add caching, remove css

--- external-markdown.php
+++ external-markdown.php
@@ -9,37 +9,59 @@
 
 function external_markdown_shortcode($atts = array())
 {
+  $GITHUB_MARKDOWN_API = "https://api.github.com/markdown";
+  $MARKDOWN_EXAMPLE = "https://raw.githubusercontent.com/pReya/wordpress-external-markdown/main/README.md";
+  // 1 hour = 60s * 60
+  $DEFAULT_CACHE_TTL = strval(60 * 60);
+
   extract(shortcode_atts(array(
-    'url' => 'https://raw.githubusercontent.com/othneildrew/Best-README-Template/master/BLANK_README.md'
+    'url' => $MARKDOWN_EXAMPLE,
+    'class' => 'external-markdown',
+    'ttl' => $DEFAULT_CACHE_TTL
   ), $atts));
 
-  $wp_remote = wp_remote_get($url);
-  $response_body = wp_remote_retrieve_body($wp_remote);
-  $response_code = wp_remote_retrieve_response_code($wp_remote);
-
-  if ($response_code != 200) {
-    return "Error: Could not load external markdown file.";
+  // TTL != 0 means caching is enabled
+  if ($ttl !== strval(0)) {
+    $cache_key = "external_markdown_" . md5($url . $class . $ttl);
+    $cached = get_transient($cache_key);
   }
 
-  $args = array(
-    'body' => json_encode(array(
-      "text" => $response_body
-    )),
-    'headers' => array(
-      'Content-Type' => 'application/json'
-    )
-  );
+  if (!(isset($cached)) || ($cached === false)) {
+    $fetch_content = wp_remote_get($url);
+    $content_response_body = wp_remote_retrieve_body($fetch_content);
+    $content_response_code = wp_remote_retrieve_response_code($fetch_content);
 
-  $response = wp_remote_post("https://api.github.com/markdown", $args);
-  $html_body = wp_remote_retrieve_body($response);
+    if ($content_response_code != 200) {
+      return "<strong>Plugin Error:</strong> Could not fetch external markdown source.";
+    }
 
-  $html_string = '<div class="markdown-body">' . $html_body . '</div>';
+    $args = array(
+      'body' => json_encode(array(
+        "text" => $content_response_body
+      )),
+      'headers' => array(
+        'Content-Type' => 'application/json'
+      )
+    );
 
-  return $html_string;
+    $fetch_github = wp_remote_post($GITHUB_MARKDOWN_API, $args);
+    $github_response_body = wp_remote_retrieve_body($fetch_github);
+    $github_response_code = wp_remote_retrieve_response_code($fetch_github);
+
+    if ($github_response_code != 200) {
+      return "<strong>Plugin Error:</strong> Could not fetch converted markdown file.";
+    }
+
+    $html_string = '<div class="' . $class . '">' . $github_response_body . '</div>';
+
+    if ($ttl != 0) {
+      set_transient($cache_key, $html_string, $ttl);
+    }
+
+    return $html_string;
+  } else {
+    return $cached;
+  }
 }
-
-add_action('wp_enqueue_style', function () {
-  wp_enqueue_style('external-markdown', plugins_url('css/external-markdown.css', __FILE__));
-});
 
 add_shortcode('external_markdown', 'external_markdown_shortcode');
